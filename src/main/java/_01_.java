9a21f616033cdd2978762e600620599535e29801
zhanxuefeng <yatolove@163.com>
2021-05-12 21:01:43 +0800

    1.整理线性表

---
+++ src/main/java/_01_.java
@@ -0,0 +1,14 @@
+public class _01_ {
+    public static void test(int n, int k){
+        for(int i = 0; i < n; i++){
+            System.out.println("test");
+        }
+        for (int i = 0; i < k; i++){
+            System.out.println("test");
+        }
+    }
+    public static int fib1(int n) {
+        if (n <= 1) return n;
+        return fib1(n - 1) + fib1(n - 2);
+    }
+}
